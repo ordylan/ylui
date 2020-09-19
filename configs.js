e4030c91d06789ebe45f545f666e7a26eb9fa687
ordylan <56828391+ordylan@users.noreply.github.com>
2020-09-19 20:42:14 +0800

    Add files via upload

--- configs.js
+++ configs.js
@@ -1,20 +1,20 @@
 YL.static = {
   /** “关于”信息 */
-  softwareName: 'YLUI DEMO', //网站名。请在此处填写您自己的网站名，如王小明的博客。
+  softwareName: '橙子小电脑', 
   version: "1.0.0", // 网站版本号
-  iconBtnStart: 'html5', //主图标
-  author: 'null',//作者
-  contactInformation: 'null',//联系方式
-  officialWebsite: '//null',//软件官网
-  welcome: '本网站UI由 YLUI 强力驱动\n更多信息：//ylui.yuri2.cn',//加载完毕控制台提示信息
-  copyrightDetail: 'null',//版权详细信息
+  iconBtnStart: '', //主图标
+  author: '橙鸭',//作者
+  contactInformation: '见主站',//联系方式
+  officialWebsite: 'https://ordylan.github.io/',//软件官网
+  welcome: '哈哈哈',//加载完毕控制台提示信息
+  copyrightDetail: '无',//版权详细信息
   otherStatements: '',//其他信息（可留空）
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
   /** YLUI基础设置 */
   lang: 'zh-cn', //语言
   localStorageName: "ylui-storage", //ls存储名
-  lockedApps: ['yl-system', 'yl-color-picker', 'ylui-fa', 'yl-browser', 'yl-server'], // 锁定的应用（不允许被脚本修改）
+  lockedApps: ['yl-system', 'yl-color-picker', 'yl-browser'], // 锁定的应用（不允许被脚本修改）
   trustedApps: ['yl-server'], // 受信任的应用（可以使用敏感API）
   debug: false,//启用更多调试信息
   beforeOnloadEnable: true,//启用关闭前询问（打包app时请关闭防止出错）
